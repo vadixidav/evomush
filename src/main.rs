2b383b1bfb34566409e8de3cc18b7faec2eff039
Geordon Worley <vadixidav@gmail.com>
2017-04-14 20:53:56 -0400

    add some initial crates, add debugger to gitignore, and add sdl demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate rand;
+extern crate heapsize;
+extern crate sdl2;
+extern crate gapush;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
-    
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer().build().unwrap();
+
+    renderer.set_draw_color(Color::RGB(255, 0, 0));
+    renderer.clear();
+    renderer.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+    }
 }
