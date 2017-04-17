92a5af8e20780fcf7c1cba456c4d4c94debcf311
Geordon Worley <vadixidav@gmail.com>
2017-04-17 18:33:56 -0400

    added all sorts of delta processing

--- src/cell/brain.rs
+++ src/cell/brain.rs
@@ -17,27 +17,34 @@ const CONNECTION_ELASTICITY_LEN: usize = 128;
 const CONNECTION_ELASTICITY_CROSSOVERS: usize = 4;
 const CONNECTION_SIGNAL_LEN: usize = 128;
 const CONNECTION_SIGNAL_CROSSOVERS: usize = 4;
-const NEIGHBOR_DETECT_LEN: usize = 128;
-const NEIGHBOR_DETECT_CROSSOVERS: usize = 4;
+const CONNECTION_SEVER_LEN: usize = 128;
+const CONNECTION_SEVER_CROSSOVERS: usize = 4;
 const REPULSION_LEN: usize = 128;
 const REPULSION_CROSSOVERS: usize = 4;
+const DIE_LEN: usize = 128;
+const DIE_CROSSOVERS: usize = 4;
+const DIVIDE_LEN: usize = 128;
+const DIVIDE_CROSSOVERS: usize = 4;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Genome {
     /// Runs to initialize the cell; this ignores any yielded instructions.
     init: Chromosome,
-    /// Runs each cycle before anything else; is passed the position of this cell in x,y and
-    /// the cell's energy on the float stack.
+    /// Runs each cycle before anything else; is passed the cell's energy on the float stack.
     cycle: Chromosome,
     /// Determines connection elasticity; this is passed the connection instruction on the instruction stack
     /// and the length on the float stack.
     connection_elasticity: Chromosome,
     /// Determines connection signal; this is to be executed directly after connection_elasticity.
     connection_signal: Chromosome,
-    /// Passes a nearby cell's data in each cycle; this includes position and energy.
-    neighbor_detect: Chromosome,
-    /// Doesn't pass anything, but tries to get a float back which indicates the cell repulsion magnitude.
+    /// Determines if the connection will be severed; this is to be executed directly after connection_signal.
+    connection_sever: Chromosome,
+    /// Doesn't pass anything, but tries to get an i64 back which indicates the cell repulsion magnitude.
     repulsion: Chromosome,
+    /// Doesn't pass anything, but tries to get a bool which determines if the cell should die.
+    die: Chromosome,
+    /// Doesn't pass anything, but tries to get a bool which determines if the cell should divide.
+    divide: Chromosome,
 
     lambda: f64,
 }
@@ -53,10 +60,12 @@ impl Genome {
             connection_signal: Chromosome::new_rand(rng,
                                                     CONNECTION_SIGNAL_LEN,
                                                     CONNECTION_SIGNAL_CROSSOVERS),
-            neighbor_detect: Chromosome::new_rand(rng,
-                                                  NEIGHBOR_DETECT_LEN,
-                                                  NEIGHBOR_DETECT_CROSSOVERS),
+            connection_sever: Chromosome::new_rand(rng,
+                                                    CONNECTION_SEVER_LEN,
+                                                    CONNECTION_SEVER_CROSSOVERS),
             repulsion: Chromosome::new_rand(rng, REPULSION_LEN, REPULSION_CROSSOVERS),
+            die: Chromosome::new_rand(rng, DIE_LEN, DIE_CROSSOVERS),
+            divide: Chromosome::new_rand(rng, DIVIDE_LEN, DIVIDE_CROSSOVERS),
             lambda: DEFAULT_LAMBDA,
         }
     }
@@ -78,8 +87,10 @@ impl Genome {
         self.connection_elasticity
             .mutate(MAXIMUM_MUTATES, &exp, rng);
         self.connection_signal.mutate(MAXIMUM_MUTATES, &exp, rng);
-        self.neighbor_detect.mutate(MAXIMUM_MUTATES, &exp, rng);
+        self.connection_sever.mutate(MAXIMUM_MUTATES, &exp, rng);
         self.repulsion.mutate(MAXIMUM_MUTATES, &exp, rng);
+        self.die.mutate(MAXIMUM_MUTATES, &exp, rng);
+        self.divide.mutate(MAXIMUM_MUTATES, &exp, rng);
     }
 
     fn mate(&self, other: &Self) -> Self {
@@ -89,8 +100,10 @@ impl Genome {
             connection_elasticity: self.connection_elasticity
                 .mate(&other.connection_elasticity),
             connection_signal: self.connection_signal.mate(&other.connection_signal),
-            neighbor_detect: self.neighbor_detect.mate(&other.neighbor_detect),
+            connection_sever: self.connection_sever.mate(&other.connection_sever),
             repulsion: self.repulsion.mate(&other.repulsion),
+            die: self.die.mate(&other.die),
+            divide: self.divide.mate(&other.divide),
             lambda: (self.lambda + other.lambda) * 0.5,
         }
     }
@@ -101,8 +114,10 @@ impl Genome {
             .and_then(|n| n.checked_sub(self.cycle.gene_len()))
             .and_then(|n| n.checked_sub(self.connection_elasticity.gene_len()))
             .and_then(|n| n.checked_sub(self.connection_signal.gene_len()))
-            .and_then(|n| n.checked_sub(self.neighbor_detect.gene_len()))
+            .and_then(|n| n.checked_sub(self.connection_sever.gene_len()))
             .and_then(|n| n.checked_sub(self.repulsion.gene_len()))
+            .and_then(|n| n.checked_sub(self.die.gene_len()))
+            .and_then(|n| n.checked_sub(self.divide.gene_len()))
             .unwrap_or(0)
     }
 }
@@ -149,6 +164,51 @@ impl Brain {
 
     pub fn set_size(&mut self, size: usize) {
         self.machine.state.max_size = self.genome.leftover_size_from(size);
+    }
+
+    /// Runs the cycle and returns the number of cycles executed.
+    pub fn run_cycle(&mut self, energy: f64) -> usize {
+        self.machine.state.push_float(energy).ok();
+        self.machine.provide_and_cycle_until(INIT_EXECUTION_TIME, (&self.genome.cycle).into()).1
+    }
+
+    /// Runs the connection chromosomes. Gives back an i64 that corresponds to the desired elasticity and
+    /// an instruction corresponding to the signal.
+    pub fn run_connection(&mut self, length: f64, ins: SimpleInstruction) ->
+            (Option<i64>, Option<SimpleInstruction>, Option<bool>, usize) {
+        self.machine.state.push_float(length).ok();
+        self.machine.state.push_ins(ins).ok();
+        let (elasticity, elen) = self.machine.provide_and_cycle_until(INIT_EXECUTION_TIME,
+                                (&self.genome.connection_elasticity).into());
+        let (signal, slen) = self.machine.provide_and_cycle_until(INIT_EXECUTION_TIME,
+                                (&self.genome.connection_signal).into());
+        let (sever, svlen) = self.machine.provide_and_cycle_until(INIT_EXECUTION_TIME,
+                                (&self.genome.connection_sever).into());
+        (   elasticity.and_then(|ins| match ins {SimpleInstruction::Pushi64(n) => Some(n), _ => None}),
+            signal,
+            sever.and_then(|ins| match ins {SimpleInstruction::Pushb(b) => Some(b), _ => None}),
+            elen + slen + svlen)
+    }
+
+    /// Runs the repulsion chromosome. Gets an i64 back that indicates the desired repulsion.
+    pub fn run_repulsion(&mut self) -> (Option<i64>, usize) {
+        let (repulsion, len) = self.machine.provide_and_cycle_until(INIT_EXECUTION_TIME,
+                                (&self.genome.repulsion).into());
+        (repulsion.and_then(|ins| match ins {SimpleInstruction::Pushi64(n) => Some(n), _ => None}), len)
+    }
+
+    /// Runs the die chromosome. Gets a bool back that indicates whether to die or not.
+    pub fn run_die(&mut self) -> (Option<bool>, usize) {
+        let (die, len) = self.machine.provide_and_cycle_until(INIT_EXECUTION_TIME,
+                                (&self.genome.die).into());
+        (die.and_then(|ins| match ins {SimpleInstruction::Pushb(b) => Some(b), _ => None}), len)
+    }
+
+    /// Runs the divide chromosome. Gets a bool back that indicates whether to divide or not.
+    pub fn run_divide(&mut self) -> (Option<bool>, usize) {
+        let (divide, len) = self.machine.provide_and_cycle_until(INIT_EXECUTION_TIME,
+                                (&self.genome.divide).into());
+        (divide.and_then(|ins| match ins {SimpleInstruction::Pushb(b) => Some(b), _ => None}), len)
     }
 }
 
--- src/cell/mod.rs
+++ src/cell/mod.rs
@@ -3,9 +3,12 @@ mod brain;
 use zoom::*;
 use nalgebra as na;
 use rand::Rng;
+use gapush::simple::{Chromosome, SimpleInstruction, PlainOp};
 
 const INIT_ENERGY: usize = 1 << 20;
 const SIZE_TO_ENERGY_RATIO: f64 = 0.05;
+const ENERGY_TO_EXECUTION_RATIO: f64 = 20.0;
+const CELL_SIGMOID_COEFFICIENT: f64 = 0.01;
 
 pub struct Cell {
     energy: usize,
@@ -32,8 +35,78 @@ impl Cell {
     pub fn energy(&self) -> usize {
         self.energy
     }
+
+    pub fn create_state(&self, connections: Vec<ConnectionState>) -> StateParameters {
+        StateParameters {
+            position: self.particle.position,
+            energy: self.energy,
+            connections: connections,
+        }
+    }
+
+    pub fn cycle(&mut self, state: StateParameters) -> Delta {
+        let cycle_cycles = self.brain.run_cycle(state.energy as f64);
+        let (connection_deltas, connection_cycles) = state.connections.into_iter()
+            .map(|cs| self.brain.run_connection(cs.length, cs.incoming))
+            .map(|(elasticity, signal, sever, cycles)|
+                (cell_sigmoid(elasticity.unwrap_or(0)),
+                signal.unwrap_or(SimpleInstruction::PlainOp(PlainOp::Nop)),
+                sever.unwrap_or(false),
+                cycles))
+            .map(|(elasticity, signal, sever, cycles)| (ConnectionDelta{
+                elasticity: elasticity,
+                signal: signal,
+                sever: sever,
+            },
+            cycles))
+            .fold((Vec::new(), 0), |(mut v, tcycles), (delta, cycles)| {v.push(delta); (v, tcycles + cycles)});
+        let (repulsion, repulsion_cycles) = self.brain.run_repulsion();
+        let repulsion = cell_sigmoid(repulsion.unwrap_or(0));
+        let (die, die_cycles) = self.brain.run_die();
+        let die = die.unwrap_or(false);
+        let (divide, divide_cycles) = self.brain.run_divide();
+        let divide = divide.unwrap_or(false);
+        self.energy = self.energy.checked_sub((ENERGY_TO_EXECUTION_RATIO *
+            (cycle_cycles + connection_cycles + repulsion_cycles + die_cycles) as f64) as usize).unwrap_or(0);
+        Delta{
+            connections: connection_deltas,
+            repulsion: repulsion,
+            // Also consider that an energy of 0 indicates death.
+            die: die || self.energy == 0,
+            divide: divide,
+        }
+    }
+}
+
+pub struct ConnectionState {
+    pub incoming: SimpleInstruction,
+    pub length: f64,
+}
+
+pub struct StateParameters {
+    pub position: na::Vector2<f64>,
+    pub energy: usize,
+    pub connections: Vec<ConnectionState>,
+}
+
+pub struct ConnectionDelta {
+    pub elasticity: f64,
+    pub signal: SimpleInstruction,
+    pub sever: bool,
+}
+
+pub struct Delta {
+    pub connections: Vec<ConnectionDelta>,
+    pub repulsion: f64,
+    pub die: bool,
+    pub divide: bool,
 }
 
 fn energy_to_size(energy: usize) -> usize {
     (energy as f64 * SIZE_TO_ENERGY_RATIO) as usize
+}
+
+fn cell_sigmoid(n: i64) -> f64 {
+    let t = n as f64 * CELL_SIGMOID_COEFFICIENT;
+    1.0 / (1.0 + (-t).exp())
 }
