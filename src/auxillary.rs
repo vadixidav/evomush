5b2fde10d296448826bb3c6c9693253c2ed21066
Geordon Worley <vadixidav@gmail.com>
2017-06-12 02:01:03 -0400

    make connection strength better

--- src/auxillary.rs
+++ src/auxillary.rs
@@ -10,8 +10,8 @@ use petgraph::Direction;
 use itertools::Itertools;
 
 // Spring attraction force.
-const HOOKE_DYNAMIC: f64 = 0.0001;
-const HOOKE_STATIC: f64 = 0.0001;
+const HOOKE_DYNAMIC: f64 = 0.001;
+const HOOKE_STATIC: f64 = 0.001;
 // Gravitational repeling force.
 const NEWTON_DYNAMIC: f64 = 10000.0;
 const NEWTON_STATIC: f64 = 10000.0;
